ac1950966a1685234453fc6391f4f6cef5fa7d04
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-05-04 02:31:20 +0300

    chore: wrapping controllers and middlerware by catchAsync

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,190 +2,156 @@ const User = require('../models/userModel.js');
 const { AppError } = require("../utils/AppError.js");
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
+const catchAsync = require('../utils/catchAsync.js');
 
 const userController = {
-  signUp: async (req, res, next) => {
+  signUp: catchAsync(async (req, res, next) => {
     const { username, email, password } = req.body;
-    try {
-      const newUser = await User.create({
-        username,
-        email,
-        password
-      });
-      if (!newUser) {
-        return next(new AppError('User not created', 400));
-      }
-      return res.status(201).json({
-        status: 'success',
-        message: 'User created successfully'
-      });
-    } catch (error) {
-      console.error("Error creating user: ", error);
-      return next(new AppError('Internal server error', 500));
+    const newUser = await User.create({
+      username,
+      email,
+      password
+    });
+    if (!newUser) {
+      return next(new AppError('User not created', 400));
     }
-  },
+    return res.status(201).json({
+      status: 'success',
+      message: 'User created successfully'
+    });
+  }),
 
-  login: async (req, res, next) => {
+  login: catchAsync(async (req, res, next) => {
     const { email, password } = req.body;
-    try {
-      const user = await User.findOne({ email }).select('+password');
-      if (!user){
-        return next(new AppError('Invalid email or password', 401));
-      }
-      const isPasswordCorrect = await user.correctPassword(password, user.password);
-      if (!isPasswordCorrect) {
-        return next(new AppError('Invalid email or password', 401));
-      }
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-      });
-
-      const refreshToken = jwt.sign( { userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN });
-
-      res.cookie('jwt', refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'Strict',
-        maxAge: process.env.MAX_AGE * 1000
-      });
-
-      return res.status(200).json({
-        status: 'success',
-        accessToken: token,
-        user: {
-          id: user._id,
-          username: user.username,
-          email: user.email,
-          photo: user.photo
-        }
-      });
-    } catch (error) {
-      console.error("Error logging in user: ", error);
-      return next(new AppError('Internal server error', 500));
+    const user = await User.findOne({ email }).select('+password');
+    if (!user){
+      return next(new AppError('Invalid email or password', 401));
     }
-  },
-
-  refreshToken: async (req, res, next) => {
-    try {
-      const refreshToken = req.cookies.jwt;
-
-      if (!refreshToken) {
-        return next(new AppError('No refresh token found', 401));
-      }
-
-      const decoded = await promisify(jwt.verify)(refreshToken, process.env.JWT_REFRESH_SECRET);
-
-      const user = await User.findById(decoded.userId);
-      if (!user) {
-        return next(new AppError('User not found', 404));
-      }
-
-      const accessToken = jwt.sign( { userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
-
-      return res.status(200).json({
-        status: 'success',
-        accessToken,
-        user: {
-          id: user._id,
-          username: user.username,
-          email: user.email,
-          photo: user.photo
-        }
-      });
-    } catch (error) {
-      console.error("Error refreshing token:", error);
-      return next(new AppError('Internal server error', 500));
+    const isPasswordCorrect = await user.correctPassword(password, user.password);
+    if (!isPasswordCorrect) {
+      return next(new AppError('Invalid email or password', 401));
     }
-  },
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN
+    });
 
-  logout: async (req, res, next) => {
-    try {
-      res.clearCookie('jwt', {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-      });
-      return res.status(200).json({
-        status: 'success',
-        message: 'Successfully logged out'
-      });
-    } catch (error) {
-      console.error("Error during logout:", error);
-      return next(new AppError('Internal server error', 500));
+    const refreshToken = jwt.sign( { userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN });
+
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'Strict',
+      maxAge: process.env.MAX_AGE * 1000
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      accessToken: token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        photo: user.photo
+      }
+    });
+  }),
+
+  refreshToken: catchAsync(async (req, res, next) => {
+    const refreshToken = req.cookies.jwt;
+
+    if (!refreshToken) {
+      return next(new AppError('No refresh token found', 401));
     }
-  },
 
-  updateUser: async (req, res, next) => {
+    const decoded = await promisify(jwt.verify)(refreshToken, process.env.JWT_REFRESH_SECRET);
+
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return next(new AppError('User not found', 404));
+    }
+
+    const accessToken = jwt.sign( { userId: user._id }, process.env.JWT_ACCESS_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN });
+
+    return res.status(200).json({
+      status: 'success',
+      accessToken,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        photo: user.photo
+      }
+    });
+  }),
+
+  logout: catchAsync(async (req, res, next) => {
+    res.clearCookie('jwt', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    });
+    return res.status(200).json({
+      status: 'success',
+      message: 'Successfully logged out'
+    });
+  }),
+
+  updateUser: catchAsync(async (req, res, next) => {
     const { username, email, photo } = req.body;
     const userId = req.user.id;
-    try {
-      const updatedUser = await User.findByIdAndUpdate(userId, {
-        username,
-        email,
-        photo
-      },
-      {
-        new: true,
-        runValidators: true
-      });
-      if (!updatedUser) {
-        return next(new AppError('User not updated', 404));
-      }
-      return res.status(200).json({
-        status: 'success',
-        data: {
-          user: updatedUser
-        }
-      });
-    } catch (error) {
-      console.error("Error updating user: ", error);
-      return next(new AppError('Internal server error', 500));
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      username,
+      email,
+      photo
+    },
+    {
+      new: true,
+      runValidators: true
+    });
+    if (!updatedUser) {
+      return next(new AppError('User not updated', 404));
     }
-  },
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser
+      }
+    });
+  }),
 
-  deleteUser: async (req, res, next) => {
+  deleteUser: catchAsync(async (req, res, next) => {
     const userId = req.user.id;
-    try {
-      const deletedUser = await User.findByIdAndDelete(userId);
-      if (!deletedUser) {
-        return next(new AppError('User not deleted', 404));
-      }
-      return res.status(200).json({
-        status: 'success',
-        data: null
-      });
-    } catch (error) {
-      console.error("Error deleting user: ", error);
-      return next(new AppError('Internal server error', 500));
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return next(new AppError('User not deleted', 404));
     }
-  },
+    return res.status(200).json({
+      status: 'success',
+      data: null
+    });
+  }),
 
-  updatePassword: async (req, res, next) => {
+  updatePassword: catchAsync(async (req, res, next) => {
     const { currentPassword, newPassword } = req.body;
     const userId = req.user.id;
-    try {
-      const user = await User.findById(userId).select('+password');
-      if (!user) {
-        return next(new AppError('User not found', 404));
-      }
-      const isPasswordCorrect = await user.correctPassword(currentPassword, user.password);
-      if (!isPasswordCorrect) {
-        return next(new AppError('Incorrect password', 401));
-      }
-      user.password = newPassword;
-      await user.save();
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-      });
-      return res.status(200).json({
-        status: 'success',
-        token
-      });
-    } catch (error) {
-      console.error("Error updating password: ", error);
-      return next(new AppError('Internal server error', 500));
+    const user = await User.findById(userId).select('+password');
+    if (!user) {
+      return next(new AppError('User not found', 404));
     }
-  },
+    const isPasswordCorrect = await user.correctPassword(currentPassword, user.password);
+    if (!isPasswordCorrect) {
+      return next(new AppError('Incorrect password', 401));
+    }
+    user.password = newPassword;
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN
+    });
+    return res.status(200).json({
+      status: 'success',
+      token
+    });
+  }),
 
   forgetPassword: async (req, res, next) => {
     // const { email } = req.body;
--- backend/middlewares/authenticateUser.js
+++ backend/middlewares/authenticateUser.js
@@ -3,15 +3,16 @@ const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const User = require('../models/userModel');
 const { AppError } = require("../utils/AppError.js");
+const catchAsync = require('../utils/catchAsync.js');
 
-const authenticateUser = async (req, res, next) => {
+const authenticateUser = catchAsync(async (req, res, next) => {
   let token;
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
     if (!token) {
       return next(new AppError('You are not logged in! Please log in to get access', 401));
     }
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_ACCESS_SECRET);
     const currentUser = await User.findById(decoded.userId);
     if (!currentUser) {
       return next(new AppError('The user belonging to this token no longer exits', 401));
@@ -19,6 +20,6 @@ const authenticateUser = async (req, res, next) => {
     req.user = currentUser;
     next();
   }
-}
+})
 
 module.exports = authenticateUser;
